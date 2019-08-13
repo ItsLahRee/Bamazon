d4a485c152c322b7cfd43561c979712af1ccfdd2
LahRee <Laurie.Strecker@gmail.com>
2019-08-12 23:41:21 -0700

    logic working

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -34,6 +34,48 @@ connection.connect(function(err) {
         stock_quantity+"\n");
 
       }
+      promptCustomer(res);
+    })
+  }
+
+  var promptCustomer = function(res){
+    inquirer.prompt([{
+      type: 'input',
+      name: 'choice',
+      message:"What would you like to purchase?"
+    }]).then(function(answer){
+      var correct = false;
+      for(var i=0;i<res.length;i++){
+        if(res[i].product_name==answer.choice){
+          correct = true;
+          var product=answer.choice;
+          var id=i;
+          inquirer.prompt({
+            type: "input",
+            name: "quant",
+            message: "How many would you like to buy?",
+            validate: function(value){
+              if(isNaN(value)==false){
+              return true;
+            } else {
+              return false;
+            }
+          }
+        }).then(function(answer){
+          if((res[id].stock_quantity-answer.quant)>0){
+            connection.query("UPDATE products SET stock_quantity='"
+            +(res[id].stock_quantity-answer.quant)+"'WHERE product_name='"
+            +product+"'", function(err,res2) {
+                console.log("Product Bought!");
+                makeTable();
+              })
+          } else {
+            console.log("Not a valid selection!");
+            promptCustomer(res);
+          }
+          })
+        }
+      }
     })
   }
 
